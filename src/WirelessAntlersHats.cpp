fe04ac77e0ad6656e89c576709057bf6f15d6e5a
Michael Sauder <michael@shownet.io>
2021-10-21 21:28:42 -0400

    Added sending out 30x seconds

--- src/WirelessAntlersHats.cpp
+++ src/WirelessAntlersHats.cpp
@@ -9,23 +9,37 @@
 // 
 // **********************************************************************************
 
-#include <RFM69.h>         //get it here: https://github.com/lowpowerlab/RFM69
-#include <RFM69_ATC.h>     //get it here: https://github.com/lowpowerlab/RFM69
-#include <RFM69_OTA.h>     //get it here: https://github.com/lowpowerlab/RFM69
-#include <SPIFlash.h>      //get it here: https://github.com/lowpowerlab/spiflash
-#include <EEPROMex.h>      //get it here: http://playground.arduino.cc/Code/EEPROMex
+#include <RFM69_ATC.h>        // https://github.com/lowpowerlab/RFM69
+#include <RFM69_OTA.h>        // https://github.com/lowpowerlab/RFM69
+#include <RFM69.h>            // https://github.com/lowpowerlab/RFM69
+#include <SPIFlash.h>         // https://github.com/lowpowerlab/spiflash
+#include <EEPROMex.h>         // http://playground.arduino.cc/Code/EEPROMex
+#include <SafeStringReader.h> // https://www.forward.com.au/pfod/ArduinoProgramming/SafeString/index.html
+#include <millisDelay.h>      // https://www.forward.com.au/pfod/ArduinoProgramming/SafeString/index.html
+#include <LowPower.h>         // https://github.com/rocketscream/Low-Power
+//#include <time.h>
+
+// ************************************************************************************
+// The defines in this section are saved into EEPROM using the WirelessBaseTarget sketch. 
+// Uncomment here to define custom values.
+// TODO LOW PRIORITY: write code to check for prescense of custom #defines and use those
+// instead of EEPROM values.
+// *************************************************************************************
 
 //#define NODEID       203  // node ID used for this unit
-//#define NETWORKID    150
-#define GATEWAY1     1
-#define GATEWAY2     2
-#define GATEWAY3     3
-#define BROADCASTID  0
-
-//#define FREQUENCY     RF69_915MHZ
-//#define FREQUENCY_EXACT 905500000
+//#define NETWORKID    150  
+//#define GATEWAY1     1
+//#define GATEWAY2     2
+//#define FREQUENCY    RF69_915MHZ
+//#define FREQUENCY_EXACT 905500000 // Move away from the default exact 915Mhz, presumably less interference
 //#define ENCRYPTKEY  "rcmhprodrcmhprod" //16-bytes or ""/0/null for no encryption
 //#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
+//#define VERSION     1 // What version software are we running?
+
+// *****************************************************************************************
+
+#define BROADCASTID  0 // Most or all Moteino documentation says to use 255 for broadcast. 255 IS WRONG!
+
 
 #define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
 #define ATC_RSSI      -80
@@ -35,9 +49,8 @@
 #define BLINKPERIOD 250
 #define DEBUG_MODE true //uncomment to enable debug comments
 
-//#define VERSION     1 // What version software are we running?
-#define ANTLER_PIN  6 //PWM pin for controlling antler LEDs
 
+#define ANTLER_PIN  6 //PWM pin for controlling antler LEDs
 
 // *****************************************************************************************************************************
 // Setup battery monitoring
@@ -49,7 +62,7 @@ float actualBatteryVoltage; // calculated battery voltage
 
 byte nodeID; // ID of this node.
 byte codeVersion; // Code version stored in configuration
-byte currentState; // Current state of this module?
+byte currentState; // Current state of this module
 bool antlerState; // Current state of the antlers
 
 SPIFlash flash(SS_FLASHMEM, FLASH_ID);
@@ -60,8 +73,12 @@ SPIFlash flash(SS_FLASHMEM, FLASH_ID);
   RFM69 radio;
 #endif
 
-char input = 0;
-long lastPeriod = -1;
+//char input = 0; // think this isn't used anymore
+//long lastPeriod = -1; // think this isn't used anymore
+
+millisDelay sleepyTimer;
+
+
 
 // struct for EEPROM config
 struct configuration {
@@ -75,7 +92,6 @@ struct configuration {
   byte gatewayID2; // 8bit address (up to 255)
   char encryptionKey[16];
   int state;     // Just in case we want to save a state setting.
-  
 } CONFIG;
 
 // struct for packets being sent to antler hats
@@ -110,33 +126,34 @@ void setup() {
   pinMode(ANTLER_PIN, OUTPUT);
 
   EEPROM.setMaxAllowedWrites(10000);
-  EEPROM.readBlock(0, CONFIG);
+  EEPROM.readBlock(0, CONFIG); // Read in the configuration struct
   
-  nodeID = CONFIG.nodeID;
-  codeVersion = CONFIG.codeVersion;
+  nodeID = CONFIG.nodeID; // nodeID and codeVersion are used in construction of every radio packet.
+  codeVersion = CONFIG.codeVersion; // This maybe saves reading from EEPROM each time? Maybe not.
   
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(SERIAL_BAUD);
   delay(1000);
   radio.initialize(CONFIG.frequency,CONFIG.nodeID,CONFIG.networkID);
-//  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+//  radio.initialize(FREQUENCY,NODEID,NETWORKID); // comment out above line and uncomment this line to use custom values
 
-  radio.setFrequency(CONFIG.frequency_exact);
-  radio.encrypt(CONFIG.encryptionKey);
+  radio.setFrequency(CONFIG.frequency_exact); // will need to change this line for custom value
+  radio.encrypt(CONFIG.encryptionKey); // will need to change this line for custom value
 
   #ifdef ENABLE_ATC
     radio.enableAutoPower(ATC_RSSI);
   #endif
 
   if (CONFIG.isHW) {
-    radio.setHighPower(); //Only for RFM69HW/HCW. Damage may occur if enabled on non-HW/HCW nodes.
+    radio.setHighPower(); //Only for RFM69HW/HCW. Damage may occur if enabled on non-HW/HCW nodes. Antler boards are RFM69HCW.
   }
 
   if (flash.initialize())
     Serial.println("SPI Flash Init OK!");
   else
     Serial.println("SPI Flash Init FAIL!");
+  flash.sleep();
 
   Serial.print("Starting node: "); Serial.println(nodeID);
   Serial.print("On network ID: "); Serial.println(CONFIG.networkID);
@@ -178,7 +195,8 @@ float BatteryVoltage()
 
 void blinkLED(int blinkTime, int blinkNumber)
 {
-
+  // TODO LOW PRIORITY
+  // fill this in if we want to blink the on-board LED in various patterns for easier troubleshooting
 }
 
 //*************************************
@@ -193,7 +211,13 @@ void sendControllerPayload(){
   controllersPayload.vcc = BatteryVoltage();
   controllersPayload.temperature = radio.readTemperature();
 
+  // TODO MEDIUM-HIGH PRIORITY
+  // Implement a random timing of the send command so that all 85 nodes announcing state changes
+  // don't drown each other out. I believe tranmission takes roughly 5ms per node.
+  // Try basing on nodeID
+
   radio.send(0, (const void*)(&controllersPayload), sizeof(controllersPayload));
+
 } // close sendControllerPayload()
 
 //*************************************
@@ -214,12 +238,36 @@ void antlers(bool state, int level = 0) // False to turn off antlers, true to tu
   }
 } // close antlers()
 
+//************************************
+//  PowerDown function
+//************************************
+void powerDown(int wakeTime){
+  radio.sleep();
+  if(currentState == 1){
+    Serial.println("Entering 8s sleep");
+    Serial.flush();
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF) ;
+  } else {
+    Serial.println("Entering 1s sleep");
+    Serial.flush();
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+  } 
+  sleepyTimer.start(wakeTime);
+  Serial.println("Leaving Sleep...");
+}
+
+
+
 //*************************************
 // Loop                               *
 //*************************************
 
 void loop(){
 
+  if(sleepyTimer.justFinished()){
+    powerDown(50);
+  
+  }
   // Check for existing RF data
   if (radio.receiveDone()) {
 
@@ -236,10 +284,11 @@ void loop(){
     #endif
 
     // Check for a new OTA sketch. If so, update will be applied and unit restarted.
+    // KEEP THIS AT TOP OF receiveDone() LOOP.
     CheckForWirelessHEX(radio, flash, false);
     //Serial.println("Checked for OTA update");
 
-    // Check if sender wanted an ACK
+    // Check if sender wanted an ACK. Doesn't mean we processed the packet correctly internally, just that we got it.
     if (radio.ACKRequested())
     {
       radio.sendACK();
@@ -249,6 +298,9 @@ void loop(){
     }
  
     // Check if valid packet. In future perhaps add checking for different payload versions
+    //
+    // TODO HIGH PRIORITY - fix this, it's not working.
+    //
     // if (radio.DATALEN != (uint8_t)sizeof(ToAntlersPayload)) {
     //   #ifdef DEBUG_MODE
     //     int bob = sizeof(radio.DATALEN);
@@ -276,38 +328,70 @@ void loop(){
       Serial.println(antlersPayload.sleepTime);
     #endif
 
-    //And now for the real meat, what should we be doing right now?
+    // And now for the real meat, what should we be doing right now?
+    // Overall we want to determine as quickly as possible if we need to do something, or if we can go back to sleep. 
+    
+    //int nodeSleeper = 1;
+    //nodeSleeper = rand() % 10 + 1;
+    
 
-    if (antlersPayload.nodeState != currentState) {
+    //if (antlersPayload.nodeState != currentState) {
       switch (antlersPayload.nodeState) {
         case 1:
-          // This is our deep sleep state
-          // We want to tell the MCU and radio to go to sleep and, if necessary, implement a counter 
-          // that goes longer than 8 seconds (default arduino limit).
+          // TODO HIGH PRIORITY
+          // This is our deep sleep state.
+          // We want to tell the MCU and radio to go to sleep for 8 seconds, wake up for 50ms (may need to play with this timing),
+          // if nothing exciting go back to sleep. Note that the way this is currently structured I think this loop is WRONG, it's only reached
+          // when a packet is received with a state change. What happens when you receive sleep packets being sent 30x/second for 10 minutes? Not sure,
+          // think through the logic here...
+          // Note that when waking up, the sketch will continue from the point in code where it went to sleep, it does NOT restart the loop.
           antlers(false,0);
           digitalWrite(LED_BUILTIN, LOW);
+          analogWrite(ANTLER_PIN, 1);
           currentState = 1;
           sendControllerPayload();
           #ifdef DEBUG_MODE
             Serial.println("Entered state 1");
           #endif
+          powerDown(50);
+          // sleep radio
+          // sleep MCU for 8 seconds
+          // wake up radio, here or maybe a wakeup routine? Be sure not to end up in a loop that never exits, so that loop() runs properly.
+          // ???
+         //radio.sleep(); //replace with our sleep function
+         #ifdef DEBUG_MODE
+           Serial.println("I woke up");
+         #endif
+         
+        //set the 50ms wait;
           break;
         case 2:
-          // This is our wake up and pay some attention state
-          // We want to shorten our sleep time to perhaps 1 second off, 20ms on? Experiment with on time.
+          // TODO HIGH PRIORITY
+          // This is our wake up and pay some attention state. We will call this 10 seconds before we trigger the antlers ON.
+          // We want to shorten our sleep time to perhaps 1 second off, 50ms on? Experiment with on time.
           // Probably the state to go in with the handheld remotes.
-          // Probably want to go back to sleep after say 2 minutes without a state change.
-          antlers(false,0);
+          // TODO PRIORITY 2 - Probably want to go back to sleep (state 1) after say 2 minutes without receiving a state change.
+          antlers(false, 0);
           digitalWrite(LED_BUILTIN, LOW);
           currentState = 2;
           sendControllerPayload();
+          powerDown(1000);
           #ifdef DEBUG_MODE
             Serial.println("Entered state 2");
           #endif
+          // sleep radio
+          // sleep MCU for 1 second
+          // wake up radio, here or maybe a wakeup routine? Be sure not to end up in a loop that never exits, so that loop() runs properly.
+          // ???
+          radio.sleep();
+          LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+          radio.setPowerLevel(RF69_MODE_RX);
           break;
         case 3:
-          // This is our standby to do stuff really soon state. 
-          // We want to stay on full time, but go back to sleep after 2 minutes without a state change.
+          // TODO HIGH PRIORITY
+          // This is our standby to do stuff really soon state. We'll call this 2 seconds before we trigger the antlers ON.
+          // We want to stay on full time, no sleep cycle, but go back to sleep after 2 minutes without a received state change.
+          sleepyTimer.stop();
           antlers(false,0);
           digitalWrite(LED_BUILTIN, LOW);
           currentState = 3;
@@ -318,6 +402,7 @@ void loop(){
           break;
         case 4:
           // This is our GO GO GO state
+          sleepyTimer.stop();
           antlers(true,255);
           digitalWrite(LED_BUILTIN, HIGH);
           currentState = 4;
@@ -327,8 +412,18 @@ void loop(){
           #endif
           break;
         case 5:
+          // TODO MEDIUM PRIORITY
           // This is the state where we experiment with keeping the antlers on
-          // while sleeping the MCU and radio
+          // while sleeping the MCU and radio. It might actually be easy.
+          sleepyTimer.stop();
+          antlers(true,255);
+          digitalWrite(LED_BUILTIN, HIGH);
+          currentState = 5;
+          sendControllerPayload();
+          #ifdef DEBUG_MODE
+            Serial.println("Entered state 5");
+          #endif
+          powerDown(50);
           break;
         case 6:
           // This is a TBD state
@@ -341,8 +436,9 @@ void loop(){
           break;  
         case 9:
           // This is our programming state. Major power hog. Programming takes time, not sure yet if want to auto sleep
-          antlers(0,0);
+          antlers(false,0);
           digitalWrite(LED_BUILTIN, LOW);
+          flash.wakeup();
           currentState = 9;
           sendControllerPayload();
           #ifdef DEBUG_MODE
@@ -350,7 +446,7 @@ void loop(){
           #endif
           break;
       } // Close switch
-    } // close if state change
+    //} // close if state change
     //} // close if valid payload
   } // close radio receive done
 
@@ -391,7 +487,6 @@ void loop(){
   //     sendControllerPayload();
   //   } 
   // } // close serial
-
 } // close loop()
 
 
